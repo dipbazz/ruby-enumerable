5c9723dd251fbfcae8290d3a46a403c6cee80093
Dipesh Bajgain <dipbazz@gmail.com>
2020-11-18 20:15:19 +0545

    Add test case for my_each_with_index method.
    - it checks if the block is not given and is returning the enumerable or not.
    - it checks if the method returns the original array.

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,17 +2,25 @@ include Enumerable
 require_relative '../enumerable'
 
 describe Enumerable do
+    let (:arr_num) { [1,2,3,4,5] }
+
     describe "#my_each" do
-        let (:arr) { [1,2,3,4,5] }
-
         it "returns enumerables if block is not given " do
-            expect(arr.my_each).to be_kind_of(Enumerable)
+            expect(arr_num.my_each).to be_kind_of(Enumerable)
         end
 
-        it "returns block if block given" do
-            expect(arr.my_each { |i| i } ).to eql(arr) 
+        it "returns each element of array" do
+            expect(arr_num.my_each { |i| i } ).to eql(arr_num)
+        end
+    end
+
+    describe "#my_each_with_index" do
+        it "returns enumerables if block is not given" do
+            expect(arr_num.my_each_with_index).to be_kind_of(Enumerable)
         end
 
-
+        it 'returns each element with index of array' do
+            expect(arr_num.my_each_with_index { |v, i| v }).to eql(arr_num)
+        end
     end
 end
