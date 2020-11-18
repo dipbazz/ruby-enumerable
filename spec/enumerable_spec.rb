68fc24bb7c864e852c1b67f130406f409ceaff35
Div685 <divyeshpatel685@gmail.com>
2020-11-18 18:02:07 +0300

    Add my_select method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -23,4 +23,17 @@ describe Enumerable do
             expect(arr_num.my_each_with_index { |v, i| v }).to eql(arr_num)
         end
     end
+
+    describe "#my_select" do
+        it "returns enumerables if block is not given" do
+            expect(arr_num.my_select).to be_kind_of(Enumerable)
+        end
+
+        it "returns new array" do
+            new_arr = arr_num.select { |i| i.odd? }
+            
+            expect(arr_num.my_select { |i| i.odd?  }).to eql(new_arr)
+        end
+
+    end
 end
