acf4b93dd4b705f764bb244814fcba566cde54e0
Dipesh Bajgain <dipbazz@gmail.com>
2020-11-18 21:19:27 +0545

    Update enumerable_spec.rb with test case for my_all? method
    - create context for falsey and truthy values
    In falsey value:
      - checkes if an array contains false value
      - checkes if an array donot have false value
      - checkes if an array of string fails the block condition
      - checkes if an array fails with regular expression.
      - checkes if an array fails with arguments value
    
    In truthy value:
      - checks if elments passes the condition
      - checks if an array is empty.

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,6 +3,8 @@ require_relative '../enumerable'
 
 describe Enumerable do
     let (:arr_num) { [1,2,3,4,5] }
+    let (:arr_with_false) { [1, 2, 3, 4, 5, nil]}
+    let (:arr_string) { %w[this is a test string]}
 
     describe "#my_each" do
         it "returns enumerables if block is not given " do
@@ -31,9 +33,47 @@ describe Enumerable do
 
         it "returns new array" do
             new_arr = arr_num.select { |i| i.odd? }
-            
+
             expect(arr_num.my_select { |i| i.odd?  }).to eql(new_arr)
         end
 
+    end
+
+    describe '#my_all?' do
+        context 'when its falsey' do
+            it 'has false value in array' do
+                expect(arr_with_false.my_all?).to eql(false)
+            end
+
+            it 'has no false value in array' do
+                expect(arr_num.my_all?).not_to eql(false)
+            end
+
+            it 'fails condition by one element in array' do
+                expect(arr_string.my_all? { |word| word.length >= 3 }).to eql(false)
+            end
+
+            it 'fails regular expression by any one element' do
+                expect(arr_string.my_all?(/t/)).to eql(false)
+            end
+
+            it 'fails the condition by any one element in array' do
+                expect(arr_string.my_all? { |word| word.length >= 3 }).to eql(false)
+            end
+
+            it 'fails any element to be numeric value' do
+                expect(arr_string.my_all?(Numeric)).to eql(false)
+            end
+        end
+
+        context 'when its truthy' do
+            it 'satisfies condition by every element in array' do
+                expect(arr_string.my_all? { |word| word.length >= 1 }).to eql(true)
+            end
+
+            it 'is an empty array' do
+                expect([].my_all?).to eql(true)
+            end
+        end
     end
 end
